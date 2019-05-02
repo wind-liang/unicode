f6ca4576cc6370662655f14b64fcf54d2c774e86
windliang <648917857@qq.com>
2019-05-02 23:21:20 +0800

    init

---
+++ index.js
@@ -0,0 +1,49 @@
+Vue.use(Toasted);
+var main = new Vue({
+    el: '#input-text',
+    data: {
+        message: '',
+        status: false
+    },
+    computed: {
+        reversedMessage: function () {
+            var newMessage = '';
+            if (this.message.length > 0) {
+                this.status = true;
+            }
+            for (var codePoint of this.message) {
+                codePoint = codePoint.charCodeAt(0);
+                // A - Z 转换
+                if (codePoint >= 65 && codePoint <= 90) {
+                    codePoint += 120315;
+                    newMessage += String.fromCodePoint(codePoint);
+                }
+                // a - z 转换
+                else if (codePoint >= 97 && codePoint <= 122) {
+                    codePoint += 120309;
+                    newMessage += String.fromCodePoint(codePoint);
+                } else {
+                    newMessage += String.fromCodePoint(codePoint);
+                }
+            }
+            return newMessage;
+        }
+    }
+});
+
+var clipboard = new ClipboardJS('.btn');
+// 成功回调
+clipboard.on('success', function (e) { 
+    Vue.toasted.show('复制成功', {
+        position: 'bottom-center',
+        duration: 2000
+    });
+    e.clearSelection();
+});
+// 失败回调
+clipboard.on('error', function (e) {
+    Vue.toasted.show('复制失败', {
+        position: 'bottom-center',
+        duration: 2000
+    });
+});
